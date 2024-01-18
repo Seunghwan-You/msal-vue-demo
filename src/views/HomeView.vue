d3b00e998d8c1a930b4f7c5add21d836e700fc64
Seunghwan,Yu <133190752+Seunghwan-You@users.noreply.github.com>
2024-01-18 23:33:17 +0900

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Home</h1>
   <p v-if="auth.account">Welcome!</p>
-  <p v-else>Click to <router-link to="/login">login</router-link>.</p>
+  <p v-else>Click to <router-link to="/login">login !!</router-link>.</p>
 </template>
 
 <script lang="ts" setup>
